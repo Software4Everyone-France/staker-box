21f1596f0f234821b1abceefb0eb4294000723d0
yohan-S4E <yohan@software4everyone.fr>
2020-12-08 12:40:48 +0100

    Fix/clean updater.sh

--- builder/updater.sh
+++ builder/updater.sh
@@ -3,7 +3,7 @@
 if [ -z "$1" ]
   then
     echo "Provide staker destination 'account@ip'"
-    exit -1
+    exit
 fi
 
 
@@ -15,22 +15,22 @@ mkdir -p out/beacon out/validator out/slasher
 
 echo "Update Prysm"
 
-cd ~/git/prysm
+cd ~/git/prysm || exit
 git pull
 
 ./prysm.sh beacon-chain --download-only
 ./prysm.sh validator --download-only
 ./prysm.sh slasher --download-only
 
-echo "Dispatch latest $(ls dist/beacon-chain-*-linux-amd64 | tail -1) file to out"
+echo "Dispatch latest $(find dist/ -maxdepth 1 -name 'beacon-chain-*-linux-amd64' | sort -r | head -n 1) file to out"
 
-cp $(ls dist/beacon-chain-*-linux-amd64 | tail -1) ${updateDir}/out/beacon/
-cp $(ls dist/validator-*-linux-amd64 | tail -1) ${updateDir}/out/validator/
-cp $(ls dist/slasher-*-linux-amd64 | tail -1) ${updateDir}/out/slasher/
+cp "$(find dist/ -maxdepth 1 -name 'beacon-chain-*-linux-amd64' | sort -r | head -n 1)"  "${updateDir}"/out/beacon/
+cp "$(find dist/ -maxdepth 1 -name 'beacon-validator-*-linux-amd64' | sort -r | head -n 1)" "${updateDir}"/out/validator/
+cp "$(find dist/ -maxdepth 1 -name 'beacon-slasher-*-linux-amd64' | sort -r | head -n 1)" "${updateDir}"/out/slasher/
 
 echo "Update Lighthouse"
 
-cd ~/git/lighthouse
+cd ~/git/lighthouse || exit
 
 echo "   update rust"
 rustup update stable
@@ -40,20 +40,20 @@ readonly lighthouseVersion=$(git describe --tags)
 make
 
 echo "Dispatch lighthouse-${lighthouseVersion} to out"
-cd ${updateDir}
-cp ~/git/lighthouse/target/release/lighthouse ${updateDir}/out/beacon/lighthouse-${lighthouseVersion}
-cp ~/git/lighthouse/target/release/lighthouse ${updateDir}/out/validator/lighthouse-${lighthouseVersion}
+cd "${updateDir}" || exit
+cp ~/git/lighthouse/target/release/lighthouse "${updateDir}"/out/beacon/lighthouse-"${lighthouseVersion}"
+cp ~/git/lighthouse/target/release/lighthouse "${updateDir}"/out/validator/lighthouse-"${lighthouseVersion}"
 
 echo "Update Teku"
 
-cd ~/git/teku
+cd ~/git/teku || exit
 git pull
 readonly tekuVersion=$(git describe --tags)
 ./gradlew distTar installDist
 
 echo "Dispatch teku-${tekuVersion} to out"
-cd ${updateDir}
-cp -r ~/git/teku/build/install/teku/ ${updateDir}/out/validator/teku-${tekuVersion}
+cd "${updateDir}" || exit
+cp -r ~/git/teku/build/install/teku/ "${updateDir}"/out/validator/teku-"${tekuVersion}"
 
 echo "Done"
 
@@ -64,4 +64,4 @@ echo "Done"
 #cd ${updateDir}
 #cp ~/git/eth2stats-client/eth2stats-client ${updateDir}/out
 
-scp -Cr ${updateDir}/out/* $1:~/staker-update/.
+scp -Cr "${updateDir}"/out/* "$1":~/staker-update/.
